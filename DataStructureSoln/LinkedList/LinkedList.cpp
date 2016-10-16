f8777dfc52bb058d760cf5ce86a7c66964161136
Ankit Mishra <ankit1703@gmail.com>
2016-10-16 18:30:58 +0530

    Initial Commit

--- DataStructureSoln/LinkedList/LinkedList.cpp
+++ DataStructureSoln/LinkedList/LinkedList.cpp
@@ -57,6 +57,33 @@ Node* Insert(Node *head, int data)
 	return head;
 }
 
+/*
+Insert Node at the begining of a linked list
+Initially head pointer argument could be NULL for empty list
+Node is defined as
+struct Node
+{
+int data;
+struct Node *next;
+}
+return back the pointer to the head of the linked list in the below method.
+*/
+Node* Insert(Node *head, int data)
+{
+	// Complete this method
+	Node *newNode = new Node;
+	newNode->data = data;
+	newNode->next = nullptr;
+
+	if (head == nullptr) {
+		head = newNode;
+	}
+	else {
+		newNode->next = head;
+		head = newNode;
+	}
+	return head;
+}
 
 int main()
 {
