2dd938bfef3e6cc3d2f45b16ffe251467f79bb62
Ankit Mishra <ankit1703@gmail.com>
2016-10-16 16:48:33 +0530

    Code added.

--- DataStructureSoln/LinkedList/LinkedList.cpp
+++ DataStructureSoln/LinkedList/LinkedList.cpp
@@ -25,6 +25,38 @@ void Print(Node *head)
 }
 
 
+/*
+Insert Node at the end of a linked list
+head pointer input could be NULL as well for empty list
+Node is defined as
+struct Node
+{
+int data;
+struct Node *next;
+}
+*/
+Node* Insert(Node *head, int data)
+{
+	// Complete this method  
+	Node *newNode = new Node;
+	Node *p;
+	newNode->data = data;
+	newNode->next = nullptr;
+
+	if (head == nullptr) {
+		head = newNode->next;
+		return newNode;
+	}
+
+	p = head;
+	while (p->next != nullptr) {
+		p = p->next;
+	}
+	p->next = newNode;
+	newNode->next = nullptr;
+	return head;
+}
+
 
 int main()
 {
