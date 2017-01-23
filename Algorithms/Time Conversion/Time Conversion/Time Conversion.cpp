92b3dae95e6b05c265c5cfac631f23c662b60211
Miank1703 <toankitmishra@gmail.com>
2017-01-23 14:38:17 +0530

    Initial Commit

---
+++ Algorithms/Time Conversion/Time Conversion/Time Conversion.cpp
@@ -0,0 +1,16 @@
+// Time Conversion.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int hr(0), mm(0), ss(0);
+	char ch[2];
+	cin >> ch[0] >> ch[1] >> ch[2];
+
+    return 0;
+}
+
